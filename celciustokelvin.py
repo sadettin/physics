c863ac9accff9aa4afaa5f63796c0665539418bc
Sadettin Kalenderoğlu <kalenderoglu@yandex.com.tr>
2012-12-07 07:35:12 -0800

    Create celciustokelvin.py

---
+++ celciustokelvin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+c=input("Put here the celcius degree: ")
+k=c+273
+if k<0:
+  print "Such a heat cannot be."
+else:
+  print ("%s celcius is %s kelvin."%(c,k))
+
+
+
