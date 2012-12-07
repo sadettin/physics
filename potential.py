986b0f8a6a9970076b2004767e2257b162c840e9
Sadettin Kalenderoğlu <kalenderoglu@yandex.com.tr>
2012-12-07 17:53:02 +0200

    Update potential.py

--- potential.py
+++ potential.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 print "Welcome to potential egergy calculation script..."
-m=input("Put mass of object:")
-g=10
-h=input("Put height of object:")
-potential=m*g*h
+m = input("Put mass of object:")
+g = 10
+h = input("Put height of object:")
+potential = m*g*h
 print "Potential energy of object:%s Newton" %(potential)
